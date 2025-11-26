a2dc296a9da94202bd10b9492e9018b3cf822dde
Maher BENRHOUMA <maher.bn.rhouma@gmail.com>
2025-11-26 13:34:56 +0100

    feat(registration): implement structured logging and error handling for user creation

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Dto\RegistrationDto;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,25 +18,55 @@ final class RegistrationController extends AbstractController
     #[Route('/register', name: 'user_registration', methods: ['POST'])]
     public function register(
         #[MapRequestPayload] RegistrationDto $requestDto,
+        LoggerInterface $logger,
         UserPasswordHasherInterface $passwordHasher,
         EntityManagerInterface $entityManager
     ): Response {
 
-        $user = new User();
-        $user->setEmail($requestDto->email);
-
-        $hashedPassword = $passwordHasher->hashPassword(
-            $user,
-            $requestDto->password
+        $logger->info(
+            'New Registration attempt',
+            [
+            'email' => $requestDto->email
+            ]
         );
-        $user->setPassword($hashedPassword);
 
-        $entityManager->persist($user);
-        $entityManager->flush();
+        try {
+            $user = new User();
+            $user->setEmail($requestDto->email);
 
-        return new JsonResponse(
-            ['message' => 'User registered successfully'],
-            201
-        );
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $requestDto->password
+            );
+            $user->setPassword($hashedPassword);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $logger->info(
+                'User registered successfully',
+                [
+                    'id' => $user->getId()
+                ]
+            );
+
+            return new JsonResponse(
+                ['message' => 'User registered successfully'],
+                201
+            );
+        } catch (\Exception $e) {
+            $logger->error(
+                'Registration failed',
+                [
+                    'message' => $e->getMessage(),
+                    'email' => $requestDto->email
+                ]
+            );
+
+            return new JsonResponse(
+                ['error' => 'Registration failed'],
+                500
+            );
+        }
     }
 }
